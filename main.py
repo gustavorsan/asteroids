ce324ede4bb29e96581574bb01dd822da97a6d8b
gustavorsan <gustaorsantos42@gmail.com>
2024-09-07 20:12:59 -0300

    Added player to groups

--- main.py
+++ main.py
@@ -9,15 +9,22 @@ def main():
     dt = 0
     y = SCREEN_HEIGHT / 2
     x = SCREEN_WIDTH / 2
+
+    updatable  = pygame.sprite.Group()
+    drawable   = pygame.sprite.Group()
+
+    Player.containers = (updatable,drawable)
     player = Player(x,y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)           
+        for obj in updatable:
+            obj.update(dt)          
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
 
